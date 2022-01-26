d8a50d9f40b02835533db0d11349f4b95ceb5b2d
mireiasuefra <mireia.suefra@gmail.com>
2022-01-26 10:59:36 +0100

    translation into Spanish

--- src/components/DetailCharacter.js
+++ src/components/DetailCharacter.js
@@ -1,6 +1,41 @@
 import { Link } from "react-router-dom";
 
 function DetailCharacter(props) {
+
+  //para traducir el genero
+  const renderGender = () => {
+    const gender = props.oneCharacter.gender;
+    if (gender === 'male') {
+      return 'Hombre';
+    } else if (gender === 'female') {
+      return 'Mujer';
+    } else {
+      return gender;
+    }
+  }
+//para traducir si está vivo o muerto
+  const renderAlive = () => {
+    const alive = props.oneCharacter.alive;
+    if (alive === true) {
+      return 'Vivo';
+    } else if (alive === false) {
+      return 'Muerto';
+    }
+  }
+//para traducir la especie
+  const renderSpecies = () => {
+    const species = props.oneCharacter.species;
+    if (species === 'human') {
+      return 'humano';
+    } else if (species === 'half-giant') {
+      return 'Semi-gigante';
+    }else if (species === 'werewolf') {
+      return 'Hombre lobo';
+    }else if (species === 'ghost') {
+      return 'Fantasma';
+    }
+  }
+
   return (
     <section>
       <Link to="/">VOLVER</Link>
@@ -14,6 +49,9 @@ function DetailCharacter(props) {
           alt="imagen de la card"
         ></img>
       <p>{props.oneCharacter.name}</p>
+      <p>{renderAlive()}</p>
+      <p>{renderSpecies()}</p> 
+      <p>{renderGender()}</p>
       <p>{props.oneCharacter.house}</p>
     </section>
   );
